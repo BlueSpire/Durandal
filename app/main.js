9804703b1f678181f8038a773174225dd90c2d29
EisenbergEffect <rob@bluespire.com>
2012-11-20 15:36:42 -0500

    Added some notes to the widget sample.

--- app/main.js
+++ app/main.js
@@ -4,8 +4,12 @@
     }
 });
 
-define(function (require) {    
+define(function(require) {
     var app = require('durandal/app');
+    
+    //The widget registration below is only necessary if you want to enable the short widget syntax.
+    var widget = require('durandal/widget');
+    widget.registerKind('accordion');
 
     app.start().then(function() {
         app.makeFit();
