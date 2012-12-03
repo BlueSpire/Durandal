02f23cdb546f823b666d1713f141eda889b1f680
EisenbergEffect <rob@bluespire.com>
2012-12-03 12:07:04 -0500

    Added a way to set the default title/options for message boxes.

--- app/durandal/messageBox.js
+++ app/durandal/messageBox.js
@@ -1,13 +1,16 @@
 ﻿define(function() {
     var MessageBox = function(message, title, options) {
         this.message = message;
-        this.title = title || "Application";
-        this.options = options || ["Ok"];
+        this.title = title || MessageBox.defaultTitle;
+        this.options = options || MessageBox.defaultOptions;
     };
 
     MessageBox.prototype.selectOption = function(dialogResult) {
         this.window.close(dialogResult);
     };
+
+    MessageBox.defaultTitle = "Application";
+    MessageBox.defaultOptions = ["Ok"];
 
     return MessageBox;
 });
