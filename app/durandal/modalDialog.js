e097c92aa8461db0741e7361e647c4a675f37bc9
EisenbergEffect <rob@bluespire.com>
2012-12-09 14:06:57 -0500

    Some internal improvements to modal dialog contexts

--- app/durandal/modalDialog.js
+++ app/durandal/modalDialog.js
@@ -3,66 +3,25 @@
         system = require('durandal/system'),
         viewModel = require('durandal/viewModel');
 
-    var modals = ko.observableArray([]);
-    var modalActivator = viewModel.activator().forItems(modals);
+    var contexts = {};
 
-    var defaultModalContext = {
-        currentZIndex: 1000,
-        blockoutOpacity: .2,
-        removeDelay: 200,
-        getNextZIndex: function() {
-            return ++this.currentZIndex;
+    var modalDialog = {
+        getContext: function(name) {
+            return contexts[name || 'default'];
         },
-        addHost: function (modal) {
-            var body = $('body');
-            var blockout = $('<div class="modalBlockout"></div>')
-                .css({ 'z-index': this.getNextZIndex(), 'opacity': this.blockoutOpacity })
-                .appendTo(body);
+        addContext: function(name, modalContext) {
+            modalContext.name = name;
+            modalContext.modals = ko.observableArray([]);
+            modalContext.activator = viewModel.activator().forItems(modalContext.modals);
 
-            var host = $('<div class="modalHost"></div>')
-                .css({ 'z-index': this.getNextZIndex() })
-                .appendTo(body);
+            contexts[name] = modalContext;
 
-            modal.host = host.get(0);
-            modal.blockout = blockout.get(0);
-        },
-        removeHost: function (modal) {
-            $(modal.host).css('opacity', 0);
-            $(modal.blockout).css('opacity', 0);
-
-            setTimeout(function () {
-                $(modal.host).remove();
-                $(modal.blockout).remove();
-            }, this.removeDelay);
-        },
-        afterCompose: function (parent, newChild, settings) {
-            var $child = $(newChild);
-            var width = $child.width();
-            var height = $child.height();
-
-            $child.css({
-                'margin-top': (-height / 2).toString() + 'px',
-                'margin-left': (-width / 2).toString() + 'px'
-            });
-
-            $(settings.model.modal.host).css('opacity', 1);
-        }
-    };
-
-    return {
-        modals: modals,
-        activator: modalActivator,
-        contexts: {
-            'default': defaultModalContext
-        },
-        addContext: function (name, modalContext) {
-            var helperName = 'show' + text.substr(0, 1).toUpperCase() + text.substr(1)
-            this.contexts[name] = modalContext;
-            this[helperName] = function (obj) {
+            var helperName = 'show' + name.substr(0, 1).toUpperCase() + name.substr(1);
+            this[helperName] = function(obj) {
                 return this.show(obj, name);
-            }
+            };
         },
-        createCompositionSettings: function (obj, modalContext) {
+        createCompositionSettings: function(obj, modalContext) {
             var settings = obj;
             var moduleId = system.getModuleId(obj);
 
@@ -80,16 +39,17 @@
 
             return settings;
         },
-        show: function (obj, context) {
+        show: function(obj, context) {
             var that = this;
-            var modalContext = this.contexts[context || 'default'];
+            var modalContext = contexts[context || 'default'];
             return system.defer(function(dfd) {
-                modalActivator.activateItem(obj).then(function(success) {
+                modalContext.activator.activateItem(obj).then(function(success) {
                     if (success) {
                         var modal = obj.modal = {
                             owner: obj,
+                            context: modalContext,
                             close: function(result) {
-                                modalActivator.deactivateItem(obj, true).then(function(closeSuccess) {
+                                modalContext.activator.deactivateItem(obj, true).then(function(closeSuccess) {
                                     if (closeSuccess) {
                                         modalContext.removeHost(modal);
                                         dfd.resolve(result);
@@ -98,8 +58,10 @@
                             }
                         };
 
+                        modal.settings = that.createCompositionSettings(obj, modalContext);
                         modalContext.addHost(modal);
-                        composition.compose(modal.host, that.createCompositionSettings(obj, modalContext));
+
+                        composition.compose(modal.host, modal.settings);
                     } else {
                         dfd.resolve(false);
                     }
@@ -107,4 +69,49 @@
             }).promise();
         }
     };
+
+    modalDialog.addContext('default', {
+        currentZIndex: 1000,
+        blockoutOpacity: .2,
+        removeDelay: 200,
+        getNextZIndex: function() {
+            return ++this.currentZIndex;
+        },
+        addHost: function(modal) {
+            var body = $('body');
+            var blockout = $('<div class="modalBlockout"></div>')
+                .css({ 'z-index': this.getNextZIndex(), 'opacity': this.blockoutOpacity })
+                .appendTo(body);
+
+            var host = $('<div class="modalHost"></div>')
+                .css({ 'z-index': this.getNextZIndex() })
+                .appendTo(body);
+
+            modal.host = host.get(0);
+            modal.blockout = blockout.get(0);
+        },
+        removeHost: function(modal) {
+            $(modal.host).css('opacity', 0);
+            $(modal.blockout).css('opacity', 0);
+
+            setTimeout(function() {
+                $(modal.host).remove();
+                $(modal.blockout).remove();
+            }, this.removeDelay);
+        },
+        afterCompose: function(parent, newChild, settings) {
+            var $child = $(newChild);
+            var width = $child.width();
+            var height = $child.height();
+
+            $child.css({
+                'margin-top': (-height / 2).toString() + 'px',
+                'margin-left': (-width / 2).toString() + 'px'
+            });
+
+            $(settings.model.modal.host).css('opacity', 1);
+        }
+    });
+
+    return modalDialog;
 });
