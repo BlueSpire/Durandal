8af59fd1a3c1f2463f014a4007007a1a80c687da
EisenbergEffect <rob@bluespire.com>
2012-12-25 20:55:41 -0500

    Created cmd options.

---
+++ Optimizer/Options.cs
@@ -0,0 +1,26 @@
+﻿namespace Optimizer {
+    using CommandLine;
+    using CommandLine.Text;
+
+    class Options : CommandLineOptionsBase {
+        [Option("b", "build", DefaultValue = true, HelpText = "Indicates that the optimizer should build the optimized JS application.")]
+        public bool Build { get; set; }
+
+        [Option("g", "generate", DefaultValue = false, HelpText = "Indicates that the optimizer should not build the optimized JS application, but only generate the r.js configuration file.")]
+        public bool Generate { get; set; }
+
+        [Option("s", "source", DefaultValue = "", HelpText = "The path to the folder which contains the application source.")]
+        public string ApplicationSource { get; set; }
+
+        [Option("c", "config", DefaultValue = "", HelpText = "The path to the customized r.js configuration file to be used as a base for the generated configuration.")]
+        public string ConfigurationSource { get; set; }
+
+        [Option("v", "verbose", DefaultValue = false, HelpText = "Indicates that verbose console logging should be used.")]
+        public bool Verbose { get; set; }
+
+        [HelpOption]
+        public string GetUsage() {
+            return HelpText.AutoBuild(this, x => HelpText.DefaultParsingErrorsHandler(this, x));
+        }
+    }
+}
--- Optimizer/Program.cs
+++ Optimizer/Program.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿namespace Optimizer {
+    using CommandLine;
 
-namespace Optimizer
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+    class Program {
+        static void Main(string[] args) {
+            var options = new Options();
+            if(CommandLineParser.Default.ParseArguments(args, options)) {
+                
+            }
         }
     }
 }
