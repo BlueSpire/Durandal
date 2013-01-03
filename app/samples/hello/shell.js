ec67d5d7dfc0e7f1a3656cacee0f1d4a6486bf0e
Rob Eisenberg <rob@bluespire.com>
2013-01-03 16:22:52 -0500

    Making some stuff look nicer in samples.

--- app/samples/hello/shell.js
+++ app/samples/hello/shell.js
@@ -7,10 +7,10 @@
     });
 
     return {
-        displayName: "Hello",
+        displayName: "What is your name?",
         name: name,
         sayHello: function () {
-            app.showMessage("Hello " + name(), "Greetings");
+            app.showMessage("Hello " + name() + "!", "Greetings");
         },
         canSayHello: canSayHello
     };
---
+++ skeletons/vstemplate/DurandalTemplate/DurandalTemplate/scripts/app/vendor/app.build.js
@@ -0,0 +1,44 @@
+{
+  "name": "vendor/almond-custom",
+  "inlineText": true,
+  "stubModules": [
+    "vendor/text"
+  ],
+  "paths": {
+    "text": "vendor/text"
+  },
+  "baseUrl": "C:\\Users\\Rob\\Documents\\GitHub\\Durandal\\skeletons\\vstemplate\\DurandalTemplate\\DurandalTemplate\\scripts\\app",
+  "mainConfigFile": "C:\\Users\\Rob\\Documents\\GitHub\\Durandal\\skeletons\\vstemplate\\DurandalTemplate\\DurandalTemplate\\scripts\\app\\main.js",
+  "include": [
+    "main",
+    "controllers/first",
+    "controllers/second",
+    "durandal/app",
+    "durandal/composition",
+    "durandal/dom",
+    "durandal/events",
+    "durandal/http",
+    "text!durandal/messageBox.html",
+    "durandal/messageBox",
+    "durandal/modalDialog",
+    "durandal/system",
+    "durandal/viewEngine",
+    "durandal/viewLocator",
+    "durandal/viewModel",
+    "durandal/viewModelBinder",
+    "durandal/widget",
+    "infrastructure/navigation",
+    "viewModels/shell",
+    "text!views/first.html",
+    "text!views/second.html",
+    "text!views/shell.html"
+  ],
+  "exclude": [],
+  "keepBuildDir": true,
+  "optimize": "uglify2",
+  "out": "C:\\Users\\Rob\\Documents\\GitHub\\Durandal\\skeletons\\vstemplate\\DurandalTemplate\\DurandalTemplate\\scripts\\app-built\\main.js",
+  "wrap": true,
+  "insertRequire": [
+    "main"
+  ]
+}
