3a0adcaf4884b33a26b56b74f064d351aa3f844b
Rob Eisenberg <rob@bluespire.com>
2013-01-02 20:33:35 -0500

    Giving same structure to vs template and nuget package.

---
+++ skeletons/nuget/content/Scripts/app/durandal/dom.js
@@ -0,0 +1,21 @@
+﻿define(function(require) {
+    var system = require('./system');
+    return {
+        ready: function() {
+            return system.defer(function(dfd) {
+                $(function() {
+                    dfd.resolve();
+                });
+            }).promise();
+        },
+        getElementById: function(id) {
+            return document.getElementById(id);
+        },
+        createElement: function(tagName) {
+            return document.createElement(tagName);
+        },
+        parseHTML: function(html) {
+            return $(html).get(0);
+        }
+    };
+});
