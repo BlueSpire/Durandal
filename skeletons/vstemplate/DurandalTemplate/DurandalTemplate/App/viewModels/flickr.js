bf74cceff47920a40899a1b58e252badbf8b4a62
Rob Eisenberg <rob@bluespire.com>
2013-01-07 20:26:50 -0500

    Fixing some consistency issues.

--- skeletons/vstemplate/DurandalTemplate/DurandalTemplate/App/viewModels/flickr.js
+++ skeletons/vstemplate/DurandalTemplate/DurandalTemplate/App/viewModels/flickr.js
@@ -7,24 +7,21 @@
         this.images = ko.observableArray([]);
     };
 
-    function callFlickrAPI(vm, search) {
-        return http.jsonp("http://api.flickr.com/services/feeds/photos_public.gne", { tags:search, tagmode: "any", format: "json" }, "jsoncallback").then(function (response) {
-            vm.images(response.items);
-        });
-    }
-
     //The activator created in the shell calls 'activate' on any view model that it's set to.
     //You can optionally return a promise for async activation.
     //NOTE: All Durandal's async operations return promises and so do all jQuery's ajax functions, so
     //they can be easily composed together and used in async screen activation scenarios.
     Flickr.prototype.activate = function() {
-        return callFlickrAPI(this, "mount ranier");
+        var that = this;
+        return http.jsonp('http://api.flickr.com/services/feeds/photos_public.gne', { tags: 'mount ranier', tagmode: 'any', format: 'json' }, 'jsoncallback').then(function (response) {
+            that.images(response.items);
+        });
     };
 
     //the app model allows easy display of modal dialogs by passing a view model
     Flickr.prototype.select = function (item) {
         //views are usually located by convention, but you an specify it as well
-        item.viewUrl = "views/detail";
+        item.viewUrl = 'views/detail';
         app.showModal(item);
     };
 
--- skeletons/vstemplate/DurandalTemplate/DurandalTemplate/App/viewModels/shell.js
+++ skeletons/vstemplate/DurandalTemplate/DurandalTemplate/App/viewModels/shell.js
@@ -7,6 +7,8 @@
         activeItem: viewModel.activator(),
         router: router,
         search: function () {
+            //It's really easy to show a message box.
+            //You can add custom options too. Also, it returns a promise for the user's response.
             app.showMessage('Search not yet implemented...');
         }
     };
