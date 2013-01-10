05b304d889d5a14ab3d8eac53ecc636f5e1503ff
Rob Eisenberg <rob@bluespire.com>
2013-01-10 10:46:23 -0500

    Moving spinner computed into obj def.

--- skeletons/vstemplate/DurandalTemplate/DurandalTemplate/App/viewmodels/shell.js
+++ skeletons/vstemplate/DurandalTemplate/DurandalTemplate/App/viewmodels/shell.js
@@ -6,22 +6,18 @@
     var shell = {
         activeItem: viewModel.activator(),
         router: router,
-        showSpinner: getShowSpinnerComputed(),
-        search: function () {
+        showSpinner: ko.computed({
+            read: function() {
+                return !shell.activeItem() || shell.activeItem.isActivating();
+            },
+            deferEvaluation: true
+        }),
+        search: function() {
             //It's really easy to show a message box.
             //You can add custom options too. Also, it returns a promise for the user's response.
             app.showMessage('Search not yet implemented...');
         }
     };
-    
-    function getShowSpinnerComputed() {
-        return ko.computed({
-            read: function() {
-                return !shell.activeItem() || shell.activeItem.isActivating();
-            },
-            deferEvaluation: true
-        });
-    }
     
     router.mapRoute('welcome', 'viewmodels/welcome', 'Welcome');
     router.mapRoute('flickr', 'viewmodels/flickr', 'Flickr');
