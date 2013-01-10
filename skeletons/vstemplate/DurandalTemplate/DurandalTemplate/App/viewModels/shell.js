5966b6882a605737f96b0e625f998756f8485602
Rob Eisenberg <rob@bluespire.com>
2013-01-10 09:24:31 -0500

    Moved spinner activation calc to computer on shell.

--- skeletons/vstemplate/DurandalTemplate/DurandalTemplate/App/viewModels/shell.js
+++ skeletons/vstemplate/DurandalTemplate/DurandalTemplate/App/viewModels/shell.js
@@ -6,12 +6,22 @@
     var shell = {
         activeItem: viewModel.activator(),
         router: router,
+        showSpinner:getShowSpinnerComputed(),
         search: function () {
             //It's really easy to show a message box.
             //You can add custom options too. Also, it returns a promise for the user's response.
             app.showMessage('Search not yet implemented...');
         }
     };
+    
+    function getShowSpinnerComputed() {
+        return ko.computed({
+            read: function() {
+                return !shell.activeItem() || shell.activeItem.isActivating();
+            },
+            deferEvaluation: true
+        });
+    }
     
     router.mapRoute('welcome', 'viewModels/welcome', 'Welcome');
     router.mapRoute('flickr', 'viewModels/flickr', 'Flickr');
