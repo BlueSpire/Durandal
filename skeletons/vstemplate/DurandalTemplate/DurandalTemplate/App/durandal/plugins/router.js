afb63d23d4480ffee7b20c2f136f9cc6fc2d80f4
Rob Eisenberg <rob@bluespire.com>
2013-01-11 09:19:20 -0500

    Refactoring to router.
    -Added private variables and functions.
    -Enabled greater flexibility with auto routing conversion.
    -Enabled greater flexibility with title generation.
    -Overwrote sammy log to push to durandal's log for greater control.

--- skeletons/vstemplate/DurandalTemplate/DurandalTemplate/App/durandal/plugins/router.js
+++ skeletons/vstemplate/DurandalTemplate/DurandalTemplate/App/durandal/plugins/router.js
@@ -1,11 +1,83 @@
 ﻿define(function (require) {
     var system = require('durandal/system');
-    var routesByPath = {}, routes = [], autoNavPath, sammy;
+    var routesByPath = {},
+        routes = [],
+        sammy,
+        router,
+        previousRoute,
+        cancelling = false,
+        navigationActivator,
+        navigationDefaultRoute;
     
     //NOTE: Sammy.js is not required by the core of Durandal. 
     //However, this plugin leverages it to enable navigation.
+    
+    function trySwap(item, title) {
+        navigationActivator.activateItem(item).then(function (succeeded) {
+            if (succeeded) {
+                document.title = title;
+                previousRoute = sammy.last_location[1].replace('/', '');
+            } else {
+                cancelling = true;
+                system.log('Cancelling Navigation');
+                sammy.setLocation(previousRoute);
+                cancelling = false;
+            }
+        });
+    }
 
-    return {
+    function activateRoute(route, params) {
+        var routeInfo = routesByPath[route];
+
+        if (!routeInfo) {
+            if (!router.convertRouteToModuleId) {
+                system.log('No Route Found', route, params);
+                return;
+            }
+
+            routeInfo = {
+                moduleId: router.convertRouteToModuleId(route),
+                name: router.convertRouteToName(route)
+            };
+        }
+
+        system.log('Activating Route', routeInfo, params);
+        system.acquire(routeInfo.moduleId).then(function(module) {
+            if (typeof module == "function") {
+                trySwap(new module(params), routeInfo.name);
+            } else {
+                trySwap(module, routeInfo.name);
+            }
+        });
+    }
+    
+    function handleRoute() {
+        if (cancelling) {
+            return;
+        }
+
+        var route = this.app.last_route.path.toString();
+        var params = this.params;
+
+        if (route == '/$/') {
+            if (router.convertRouteToModuleId) {
+                var fragment = this.path.split('#/');
+                if (fragment.length == 2) {
+                    var parts = fragment[1].split('/');
+                    route = parts[0];
+                    params = parts.splice(1);
+                } else {
+                    route = navigationDefaultRoute;
+                }
+            } else {
+                route = navigationDefaultRoute;
+            }
+        }
+
+        activateRoute(route, params);
+    }
+
+    return router = {
         navigationReady:ko.observable(false),
         navigation: ko.observableArray([]),
         navigateBack:function () {
@@ -14,18 +86,24 @@
         navigateTo:function (url) {
             sammy.setLocation(url);
         },
+        convertRouteToName: function (route) {
+            return route.substring(0, 1).toUpperCase() + route.substring(1);
+        },
         mapAuto: function (path) {
-            autoNavPath = path || 'viewmodels';
+            path = path || 'viewmodels';
+
+            this.convertRouteToModuleId = function(url) {
+                return path + "/" + url;
+            };
         },
         mapNav: function (url, moduleId, name) {
-            this.mapRoute(url, moduleId, name, true);
+            return this.mapRoute(url, moduleId, name, true);
         },
         mapRoute: function (url, moduleId, name, isNav) {
-            var that = this;
             var routeInfo = {
                 url: url,
                 moduleId: moduleId,
-                name: name || (url.substring(0, 1).toUpperCase() + url.substring(1))
+                name: name || this.convertRouteToName(url)
             };
 
             routesByPath[url] = routeInfo;
@@ -33,85 +111,26 @@
 
             if (isNav) {
                 routeInfo.isActive = ko.computed(function() {
-                    return that.navigationReady() && that.activator() && that.activator().__moduleId__ == routeInfo.moduleId;
+                    return router.navigationReady()
+                        && navigationActivator()
+                        && navigationActivator().__moduleId__ == routeInfo.moduleId;
                 });
 
                 this.navigation.push(routeInfo);
             }
+
+            return routeInfo;
         },
         enable: function (activator, defaultRoute) {
-            var cancelling = false, previousRoute;
-            
-            this.activator = activator;
-
-            function trySwap(item, title) {
-                activator.activateItem(item).then(function (succeeded) {
-                    if (succeeded) {
-                        document.title = title;
-                        previousRoute = sammy.last_location[1].replace('/', '');
-                    } else {
-                        cancelling = true;
-                        sammy.setLocation(previousRoute);
-                        cancelling = false;
-                    }
-                });
-            }
-
-            function activateRoute(route, params) {
-                var routeInfo = routesByPath[route];
-
-                if (!routeInfo) {
-                    if (!autoNavPath) {
-                        return;
-                    }
-
-                    routeInfo = {
-                        moduleId: autoNavPath + "/" + route,
-                        name: route.substring(0, 1).toUpperCase() + route.substring(1)
-                    };
-                }
-
-                system.acquire(routeInfo.moduleId).then(function (module) {
-                    if (typeof module == "function") {
-                        trySwap(new module(params), routeInfo.name);
-                    } else {
-                        trySwap(module, routeInfo.name);
-                    }
-                });
-            }
-
-            function handleRoute() {
-                if (cancelling) {
-                    return;
-                }
-
-                var route = this.app.last_route.path.toString();
-                var params = this.params;
-
-                if (route == '/$/') {
-                    if (autoNavPath) {
-                        var fragment = this.path.split('#/');
-                        if (fragment.length == 2) {
-                            var parts = fragment[1].split('/');
-                            route = parts[0];
-                            params = parts.splice(1);
-                        } else {
-                            route = defaultRoute;
-                        }
-                    } else {
-                        route = defaultRoute;
-                    }
-                }
-
-                activateRoute(route, params);
-            }
+            navigationActivator = activator;
+            navigationDefaultRoute = defaultRoute;
 
             sammy = Sammy(function(route) {
                 for (var i = 0; i < routes.length; i++) {
                     var current = routes[i];
 
                     if (!(current.url instanceof RegExp)) {
-                        route.get('#/' + current.url, handleRoute);
+                        route.get(current.url, handleRoute);
                     } else {
                         route.get(current.url, handleRoute);
                     }
@@ -125,6 +144,12 @@
 
             sammy._checkFormSubmission = function () {
                 return false;
+            };
+
+            sammy.log = function () {
+                var args = Array.prototype.slice.call(arguments, 0);
+                args.unshift('Sammy');
+                system.log.apply(system, args);
             };
 
             this.navigationReady(true);
