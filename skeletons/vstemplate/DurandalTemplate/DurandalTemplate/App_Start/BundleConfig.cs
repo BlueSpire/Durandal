2b5b86aa34b78f45e6d9a838b9a5c58c6d3046d3
Rob Eisenberg <rob@bluespire.com>
2013-01-24 11:11:36 -0500

    Updated nuget package info.
    Updated VS temp (non-nuget version).

--- skeletons/vstemplate/DurandalTemplate/DurandalTemplate/App_Start/BundleConfig.cs
+++ skeletons/vstemplate/DurandalTemplate/DurandalTemplate/App_Start/BundleConfig.cs
@@ -17,6 +17,7 @@ namespace DurandalTemplate {
 
       bundles.Add(
         new StyleBundle("~/Content/css")
+          .Include("~/Content/ie10mobile.css")
           .Include("~/Content/bootstrap.min.css")
           .Include("~/Content/app.css")
           .Include("~/Content/bootstrap-responsive.min.css")
