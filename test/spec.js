aa9a8540cb297dee11fcee91128cc40e11453add
Jon Curtis <jonathan.curtis@huddle.com>
2013-02-11 11:13:04 +0000

    Added travis CI configuration and fixed tests

--- test/spec.js
+++ test/spec.js
@@ -1,9 +1,22 @@
-var page = require('webpage').create();
+/*global phantom, require, runTests*/
+var fs = require('fs'),
+    page = require('webpage').create(),
+    specFiles;
+
+specFiles = fs.list('specs')
+    .filter(function(item) {
+        return item.indexOf('spec.js') !== -1;
+    })
+    .map(function(item) {
+        return 'specs/' + item.substring(0, item.length - 3);
+    });
+
+console.log('Running spec files: ' + specFiles);
 
 page.onConsoleMessage = function(msg) {
     console.log(msg);
 
-    if (msg == "ConsoleReporter finished") {
+    if (msg === "ConsoleReporter finished") {
         phantom.exit();
     }
 };
@@ -11,7 +24,7 @@ page.onConsoleMessage = function(msg) {
 page.onLoadFinished = function () {
     page.evaluate(function (specFiles) {
         runTests(specFiles);
-    }, ['specs/system.spec']);
+    }, specFiles);
 };
 
 page.open('spec.html');
