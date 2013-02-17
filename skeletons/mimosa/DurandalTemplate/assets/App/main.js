1340617546889e9a17d1821026b50d28fad3dfad
Rob Eisenberg <rob@bluespire.com>
2013-02-16 22:14:06 -0500

    Updated the mimosa main.js.

--- skeletons/mimosa/DurandalTemplate/assets/App/main.js
+++ skeletons/mimosa/DurandalTemplate/assets/App/main.js
@@ -21,6 +21,7 @@ define(function(require) {
     system.debug(true);
     //>>excludeEnd("build");
 
+    app.title = 'Durandal Starter Kit';
     app.start().then(function () {
         //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
         //Look for partial views in a 'views' folder in the root.
