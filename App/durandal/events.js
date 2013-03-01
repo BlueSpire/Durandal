6379058a2007ee7d9622b4d9a5f9e1cd2108074a
Dan Walton <dgwalton@gmail.com>
2013-03-01 06:31:58 -0600

    make Durandal compatible with the dojo AMD loader
    replace CommonJS wrapping; add checks in system.js for dojo and
    requirejs

--- App/durandal/events.js
+++ App/durandal/events.js
@@ -1,6 +1,5 @@
 ﻿//heavily borrowed from backbone events, augmented by signals.js, added a little of my own code, cleaned up for better readability
-define(function(require) {
-    var system = require('./system');
+define(['./system'], function (system) {
     var eventSplitter = /\s+/;
     var Events = function() { };
 
