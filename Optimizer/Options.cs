bb73a9c584ee603330e5b7494381644bf81e1e8e
Chris Young <cyoung@kalos-inc.com>
2013-03-01 12:49:52 -0600

    getting everything updated for commandlineparser

--- Optimizer/Options.cs
+++ Optimizer/Options.cs
@@ -3,26 +3,26 @@ using CommandLine;
 using CommandLine.Text;
 
 namespace Optimizer {
-  class Options : CommandLineOptionsBase {
-    [Option("m", "mode", DefaultValue = ModeOptions.Build, HelpText = "Indicates whether the optimizer should only 'generate' the r.js config or actually 'build' the optimized JS application.")]
+  class Options {
+    [Option('m', "mode", DefaultValue = ModeOptions.Build, HelpText = "Indicates whether the optimizer should only 'generate' the r.js config or actually 'build' the optimized JS application.")]
     public string Mode { get; set; }
 
-    [Option("s", "source", DefaultValue = "", HelpText = "The path to the folder which contains the application source.")]
+    [Option('s', "source", DefaultValue = "", HelpText = "The path to the folder which contains the application source.")]
     public string ApplicationSource { get; set; }
 
-    [Option("c", "config", DefaultValue = "", HelpText = "The path to the customized r.js configuration file to be used as a base for the generated configuration.")]
+    [Option('c', "config", DefaultValue = "", HelpText = "The path to the customized r.js configuration file to be used as a base for the generated configuration.")]
     public string ConfigurationSource { get; set; }
 
-    [Option("v", "verbose", DefaultValue = false, HelpText = "Indicates that verbose console logging should be used.")]
+    [Option('v', "verbose", DefaultValue = false, HelpText = "Indicates that verbose console logging should be used.")]
     public bool Verbose { get; set; }
 
-    [Option("l", "loader", DefaultValue = LoaderOptions.Almond, HelpText = "Indicates which script loader to optimize for 'almond' or 'require'.")]
+    [Option('l', "loader", DefaultValue = LoaderOptions.Almond, HelpText = "Indicates which script loader to optimize for 'almond' or 'require'.")]
     public string Loader { get; set; }
 
-    [Option("p", "plugin", DefaultValue = "text", HelpText = "The view plugin used to optimize views into the final build file.")]
+    [Option('p', "plugin", DefaultValue = "text", HelpText = "The view plugin used to optimize views into the final build file.")]
     public string ViewPlugin { get; set; }
 
-    [Option("e", "pluginExtension", DefaultValue = ".html", HelpText = "The view file extension.")]
+    [Option('e', "pluginExtension", DefaultValue = ".html", HelpText = "The view file extension.")]
     public string ViewExtension { get; set; }
 
     [HelpOption]
