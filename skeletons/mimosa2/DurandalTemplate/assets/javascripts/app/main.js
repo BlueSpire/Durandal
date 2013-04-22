e6cc121bab31353bcd673508be93c99dc2713f23
Rob Eisenberg <rob@bluespire.com>
2013-04-22 12:43:10 -0400

    Working more on setup of the new mimosa skeleton.

---
+++ skeletons/mimosa2/DurandalTemplate/assets/javascripts/app/main.js
@@ -0,0 +1,40 @@
+﻿require.config({
+    paths: {
+        'text': 'durandal/amd/text'
+    }
+});
+
+define(function(require) {
+    var app = require('durandal/app'),
+        viewLocator = require('durandal/viewLocator'),
+        system = require('durandal/system'),
+        router = require('durandal/plugins/router');
+
+    //This second set of requires is temporary, until we rewrite a custom mimosa module to handle it.
+    require('durandal/messageBox')
+    require('durandal/transitions/entrance')
+    require('viewmodels/shell')
+    require('viewmodels/welcome')
+    require('viewmodels/flickr')
+    
+    //>>excludeStart("build", true);
+    system.debug(true);
+    //>>excludeEnd("build");
+
+    app.title = 'Durandal Starter Kit';
+    app.start().then(function () {
+        //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
+        //Look for partial views in a 'views' folder in the root.
+        viewLocator.useConvention();
+
+        //configure routing
+        router.useConvention();
+        router.mapNav('welcome');
+        router.mapNav('flickr');
+
+        app.adaptToDevice();
+        
+        //Show the app by setting the root view model for our application with a transition.
+        app.setRoot('viewmodels/shell', 'entrance');
+    });
+});
