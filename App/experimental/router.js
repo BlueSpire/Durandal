139559db09dc47e7a6daee63adf9a5595f8d14e8
EisenbergEffect <info@bluespire.com>
2013-05-19 11:11:47 -0400

    Some work prepping new router for actual code.

--- App/experimental/router.js
+++ App/experimental/router.js
@@ -1,16 +1,15 @@
 ﻿define(['../durandal/app', './history'], function (app, history) {
-    function result() {}
-    function keys(){}
-    function isRegExp() {}
-    function isFunction() {}
-    function map() {}
-
     // Cached regular expressions for matching named param parts and splatted
     // parts of route strings.
     var optionalParam = /\((.*?)\)/g;
     var namedParam = /(\(\?)?:\w+/g;
     var splatParam = /\*\w+/g;
     var escapeRegExp = /[\-{}\[\]+?.,\\\^$|#\s]/g;
+
+    function keys(){}
+    function isRegExp() {}
+    function isFunction() {}
+    function map() {}
 
     // Convert a route string into a regular expression, suitable for matching
     // against the current location hash.
@@ -37,28 +36,7 @@
 
     // Routers map faux-URLs to actions, and fire events when routes are
     // matched. Creating a new one sets its `routes` hash, if not set statically.
-    var Router = function(options) {
-        options || (options = {});
-        
-        if (options.routes) {
-            this.routes = options.routes;
-        }
-
-        // Bind all defined routes to history. We have to reverse the
-        // order of the routes here to support behavior where the most general
-        // routes can be defined at the bottom of the route map.
-        
-        if (!this.routes) {
-            return;
-        }
-
-        this.routes = result(this, 'routes');
-        var route, routes = keys(this.routes);
-
-        while ((route = routes.pop()) != null) {
-            this.route(route, this.routes[route]);
-        }
-    };
+    var router = {};
     
     // Manually bind a single named route to a callback. For example:
     //
@@ -66,7 +44,7 @@
     //       ...
     //     });
     //
-    Router.prototype.route = function(route, name, callback) {
+    router.route = function (route, name, callback) {
         if (!isRegExp(route)) {
             route = routeToRegExp(route);
         }
@@ -77,10 +55,8 @@
         }
         
         if (!callback) {
-            callback = this[name];
+            callback = router[name];
         }
-
-        var router = this;
 
         history.route(route, function(fragment) {
             var args = extractParameters(route, fragment);
@@ -90,14 +66,43 @@
             history.trigger('route', router, name, args);
         });
 
-        return this;
+        return router;
     };
     
     // Simple proxy to `history` to save a fragment into the history.
-    Router.prototype.navigate = function (fragment, options) {
+    router.navigate = function (fragment, options) {
         history.navigate(fragment, options);
-        return this;
+        return router;
     };
 
-    return Router;
+    router.configure = function (options) {
+        options || (options = {});
+
+        if (options.routes) {
+            router.routes = options.routes;
+        }
+
+        if (!router.routes) {
+            return router;
+        }
+
+        var route, routes = keys(router.routes);
+
+        // Bind all defined routes to history. We have to reverse the
+        // order of the routes here to support behavior where the most general
+        // routes can be defined at the bottom of the route map.
+
+        while ((route = routes.pop()) != null) {
+            router.route(route, router.routes[route]);
+        }
+        
+        return router;
+    };
+
+    router.activate = function () {
+        history.start();
+        return router;
+    };
+
+    return router;
 });
