9a46444a5939d28a2fcc9aefdea1f0036fa3d2ab
EisenbergEffect <rob@bluespire.com>
2013-06-05 23:41:01 -0400

    Fixing up the raw HTML platform to resemble @dslatten's recommendation.

---
+++ platforms/HTML/StarterKit/app/main.js
@@ -0,0 +1,11 @@
+﻿requirejs.config({
+    paths: {
+        'text': '../lib/require/text',
+        'plugins' : '../lib/durandal/js/plugins',
+        'transitions' : '../lib/durandal/js/transitions'
+    }
+});
+
+define(['../lib/durandal/js/durandal'], function(){
+    require(['bootstrapper']);
+});
