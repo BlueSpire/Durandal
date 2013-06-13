ac52f6dd9718049fd05e2d4be5e9eb909e6c29d3
EisenbergEffect <rob@bluespire.com>
2013-06-13 02:13:44 -0400

    Fixed up the platform support with the new modules.

--- platforms/HTML/Samples/app/main.js
+++ platforms/HTML/Samples/app/main.js
@@ -1,11 +1,24 @@
 ﻿requirejs.config({
     paths: {
         'text': '../lib/require/text',
+        'durandal':'../lib/durandal/js',
         'plugins' : '../lib/durandal/js/plugins',
         'transitions' : '../lib/durandal/js/transitions'
     }
 });
 
-define(['../lib/durandal/js/durandal'], function(){
-    require(['bootstrapper']);
+define(['durandal/system', 'durandal/app', 'durandal/viewLocator'],  function (system, app, viewLocator) {
+    //>>excludeStart("build", true);
+    system.debug(true);
+    //>>excludeEnd("build");
+
+    app.title = 'Durandal Samples';
+    app.start().then(function () {
+        //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
+        //Look for partial views in a 'views' folder in the root.
+        viewLocator.useConvention();
+
+        //Show the app by setting the root view model for our application.
+        app.setRoot('shell');
+    });
 });
--- platforms/Microsoft.NET/Samples/Durandal.Samples/App/main.js
+++ platforms/Microsoft.NET/Samples/Durandal.Samples/App/main.js
@@ -1,11 +1,24 @@
 ﻿requirejs.config({
     paths: {
         'text': '../Scripts/text',
+        'durandal':'../Scripts/durandal',
         'plugins': '../Scripts/durandal/plugins',
         'transitions': '../Scripts/durandal/transitions'
     }
 });
 
-define(['../Scripts/durandal/durandal'], function () {
-    require(['bootstrapper']);
+define(['durandal/system', 'durandal/app', 'durandal/viewLocator'],  function (system, app, viewLocator) {
+    //>>excludeStart("build", true);
+    system.debug(true);
+    //>>excludeEnd("build");
+
+    app.title = 'Durandal Samples';
+    app.start().then(function () {
+        //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
+        //Look for partial views in a 'views' folder in the root.
+        viewLocator.useConvention();
+
+        //Show the app by setting the root view model for our application.
+        app.setRoot('shell');
+    });
 });
