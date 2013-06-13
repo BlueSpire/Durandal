0290bcd8c8b6941594b11225c273a3c7ff4b8e12
Ted Eiles <ted@showops.com>
2013-06-13 15:44:37 -0400

    Build failed on my clean machine because destination folders were not being recursively build.  I'm new to powershell so I just added for a few directory create commands and used xcopy for hierarchies of unknown depth.  Build works. http://blog.netnerds.net/2011/10/powershell-copy-item-recurse-force-does-not-create-directories-when-using-get-childitem-include/ http://stackoverflow.com/questions/5432290/how-to-use-powershell-copy-item-and-keep-structure

--- src/build.ps1
+++ src/build.ps1
@@ -6,15 +6,20 @@ function buildDurandal(){
   Write-Host "Building Durandal"
 
   $moduleDirectory = $srcDir + "/durandal/js";
+  $outputFolder= $distDir + "/durandal/js"
+  if ( -not (Test-Path $outputFolder) ) {New-Item $outputFolder  -Type Directory  | Out-Null}
 
   foreach($file in Get-ChildItem $moduleDirectory){
     $filePath = $moduleDirectory + "/" + $file;
-    $outputPath = $distDir + "/durandal/js/" + $file.name
-
+    $outputPath = $outputFolder + "/"+ $file.name
+    
     Get-Content $startFragment, $filePath | Set-Content $outputPath
   }
-
+  $outputFolder= $distDir + "/durandal/css/";
+  if ( -not (Test-Path $outputFolder) ) {New-Item $outputFolder  -Type Directory  | Out-Null}
   Copy-Item "durandal/css/durandal.css" ($distDir + "/durandal/css/")
+  $outputFolder= $distDir + "/durandal/images/";
+  if ( -not (Test-Path $outputFolder) ) {New-Item $outputFolder  -Type Directory  | Out-Null}
   Copy-Item "durandal/images/*" ($distDir + "/durandal/images/")
 }
 
@@ -22,10 +27,12 @@ function buildOptionalModules($folderName){
   Write-Host "Building $folderName"
 
   $moduleDirectory = $srcDir + "/" + $folderName + "/js";
+  $outputFolder= $distDir + "/durandal/js/" + $folderName 
+  if ( -not (Test-Path $outputFolder) ) {New-Item $outputFolder  -Type Directory  | Out-Null}
 
   foreach($file in Get-ChildItem $moduleDirectory){
       $filePath = $moduleDirectory + "/" + $file;
-      $outputPath = $distDir + "/durandal/js/" + $folderName + "/" + $file.name
+      $outputPath =  $outputFolder + "/" + $file.name
 
       Get-Content $startFragment, $filePath | Set-Content $outputPath
   }
@@ -33,12 +40,12 @@ function buildOptionalModules($folderName){
 
 function buildSamples(){
   Write-Host "Building Samples"
-  Copy-Item "samples/*" ($distDir + "/samples") -recurse -force
+  cmd /c robocopy.exe "./samples" ($distDir + "/samples") /e /np /r:1 /w:1
 }
 
 function buildStarterKit(){
   Write-Host "Building StarterKit"
-  Copy-Item "starterkit/*" ($distDir + "/starterkit") -recurse -force
+  cmd /c robocopy.exe "./starterkit" ($distDir + "/starterkit") /e /np /r:1 /w:1
 }
 
 buildDurandal
