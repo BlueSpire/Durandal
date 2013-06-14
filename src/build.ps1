bc30109ffcc45409ab882345c62f22d3d6a44623
EisenbergEffect <rob@bluespire.com>
2013-06-13 21:27:53 -0400

    Some code formatting in the src build file.

--- src/build.ps1
+++ src/build.ps1
@@ -1,5 +1,6 @@
 $srcDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 $distDir = [IO.Path]::GetFullPath( (join-path $srcDir "../dist") )
+
 Remove-Item $distDir -Force -Recurse
 
 $startFragment = $srcDir + "/start.jsfrag"
@@ -8,7 +9,8 @@ function buildDurandal(){
   Write-Host "Building Durandal"
 
   $moduleDirectory = $srcDir + "/durandal/js";
-  $outputFolder= $distDir + "/durandal/js"
+  $outputFolder = $distDir + "/durandal/js"
+
   if ( -not (Test-Path $outputFolder) ) {New-Item $outputFolder  -Type Directory  | Out-Null}
 
   foreach($file in Get-ChildItem $moduleDirectory){
@@ -17,10 +19,12 @@ function buildDurandal(){
     
     Get-Content $startFragment, $filePath | Set-Content $outputPath
   }
-  $outputFolder= $distDir + "/durandal/css/";
+
+  $outputFolder = $distDir + "/durandal/css/";
   if ( -not (Test-Path $outputFolder) ) {New-Item $outputFolder  -Type Directory  | Out-Null}
   Copy-Item "durandal/css/durandal.css" ($distDir + "/durandal/css/")
-  $outputFolder= $distDir + "/durandal/images/";
+
+  $outputFolder = $distDir + "/durandal/images/";
   if ( -not (Test-Path $outputFolder) ) {New-Item $outputFolder  -Type Directory  | Out-Null}
   Copy-Item "durandal/images/*" ($distDir + "/durandal/images/")
 }
@@ -29,7 +33,7 @@ function buildOptionalModules($folderName){
   Write-Host "Building $folderName"
 
   $moduleDirectory = $srcDir + "/" + $folderName + "/js";
-  $outputFolder= $distDir + "/durandal/js/" + $folderName 
+  $outputFolder = $distDir + "/durandal/js/" + $folderName
   if ( -not (Test-Path $outputFolder) ) {New-Item $outputFolder  -Type Directory  | Out-Null}
 
   foreach($file in Get-ChildItem $moduleDirectory){
