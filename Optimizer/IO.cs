9162e7f38e152ded541b61937ce2212031c972b1
Laurenţiu Macovei <laurentiu.macovei@dotnetwise.com>
2013-06-17 18:06:16 +0300

    Restored braces editor format

--- Optimizer/IO.cs
+++ Optimizer/IO.cs
@@ -5,45 +5,34 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Text.RegularExpressions;
 
-namespace Optimizer
-{
-    static class IO
-    {
-        public static bool ExistsOnPath(string fileName)
-        {
-            if (GetFullPath(fileName) != null)
-            {
+namespace Optimizer {
+    static class IO {
+        public static bool ExistsOnPath(string fileName) {
+            if (GetFullPath(fileName) != null) {
                 return true;
             }
             return false;
         }
 
-        public static string GetFullPath(string fileName)
-        {
-            if (File.Exists(fileName))
-            {
+        public static string GetFullPath(string fileName) {
+            if (File.Exists(fileName)) {
                 return Path.GetFullPath(fileName);
             }
 
             var values = Environment.GetEnvironmentVariable("PATH");
-            foreach (var path in values.Split(';'))
-            {
+            foreach (var path in values.Split(';')) {
                 var fullPath = Path.Combine(path, fileName);
-                if (File.Exists(fullPath))
-                {
+                if (File.Exists(fullPath)) {
                     return fullPath;
                 }
             }
             return null;
         }
 
-        public static JObject GetBaseConfiguration(Options options)
-        {
-            if (!string.IsNullOrEmpty(options.ConfigurationSource))
-            {
+        public static JObject GetBaseConfiguration(Options options) {
+            if (!string.IsNullOrEmpty(options.ConfigurationSource)) {
                 var fullPath = options.ConfigurationSource;
-                if (!Path.IsPathRooted(fullPath))
-                {
+                if (!Path.IsPathRooted(fullPath)) {
                     fullPath = Path.Combine(Directory.GetCurrentDirectory(), fullPath);
                 }
 
@@ -51,45 +40,36 @@ namespace Optimizer
             }
 
             var configPath = Path.Combine(Directory.GetCurrentDirectory(), "optimizer.base.js");
-            if (File.Exists(configPath))
-            {
+            if (File.Exists(configPath)) {
                 return ReadConfigFromFile(configPath, options);
             }
 
             return ReadConfigFromResource(options);
         }
 
-        public static Stream ReadFromResource(string name)
-        {
+        public static Stream ReadFromResource(string name) {
             return Assembly
               .GetExecutingAssembly()
               .GetManifestResourceStream("Optimizer.Resources." + name);
         }
 
-        static JObject ReadConfigFromResource(Options options)
-        {
+        static JObject ReadConfigFromResource(Options options) {
             options.Log("Using default base configuration.");
 
-            using (var stream = ReadFromResource("optimizer.base.js"))
-            {
-                using (var streamReader = new StreamReader(stream))
-                {
-                    using (var jsonReader = new JsonTextReader(streamReader))
-                    {
+            using (var stream = ReadFromResource("optimizer.base.js")) {
+                using (var streamReader = new StreamReader(stream)) {
+                    using (var jsonReader = new JsonTextReader(streamReader)) {
                         return JObject.Load(jsonReader);
                     }
                 }
             }
         }
 
-        static JObject ReadConfigFromFile(string path, Options options)
-        {
+        static JObject ReadConfigFromFile(string path, Options options) {
             options.Log("Reading configuration from " + path);
 
-            using (var reader = File.OpenRead(path))
-            {
-                using (var streamReader = new StreamReader(reader))
-                {
+            using (var reader = File.OpenRead(path)) {
+                using (var streamReader = new StreamReader(reader)) {
                     var json = streamReader.ReadToEnd();
                     //allows i.e. module.exports = { } instead of a plain json, so you'd have intellisense in VS2012
                     var index = 0;
@@ -103,14 +83,11 @@ namespace Optimizer
             }
         }
 
-        public static void WriteConfiguration(RJSConfig info, Options options)
-        {
+        public static void WriteConfiguration(RJSConfig info, Options options) {
             options.Log(info.Config);
 
-            using (var file = File.Create(info.BuildFilePath))
-            {
-                using (var writer = new StreamWriter(file))
-                {
+            using (var file = File.Create(info.BuildFilePath)) {
+                using (var writer = new StreamWriter(file)) {
                     writer.Write(info.Config.ToString());
                 }
             }
