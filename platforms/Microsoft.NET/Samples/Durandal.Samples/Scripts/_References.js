35621a3a873544e3a307d2829f677660a4036d88
Laurenţiu Macovei <laurentiu.macovei@dotnetwise.com>
2013-06-18 13:50:05 +0300

    Added Durandal.Samples/Scripts/_References.js so that intellisense would load these libraries first if properly configured see more at http://blog.craigtp.co.uk/post/Javascript-jQuery-Intellisense-in-Visual-Studio-2012.aspx

---
+++ platforms/Microsoft.NET/Samples/Durandal.Samples/Scripts/_References.js
@@ -0,0 +1,9 @@
+﻿/// <reference path="jquery-1.9.1.js" />
+/// <reference path="knockout-2.2.1.debug.js" />
+/// <reference path="bootstrap.js" />
+/// <reference path="require.js" />
+/// <reference path="../App/main.js" />
+
+//enable ko and jQuery AMD, otherwise intellisense won't work!
+define.amd = define.amd || {};
+define.amd.jQuery = true;
