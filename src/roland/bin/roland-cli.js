af37f033e0b3f2730c3bd094204532190e146b52
EisenbergEffect <rob@bluespire.com>
2013-06-20 01:27:18 -0400

    Stubbing some basics for roland cli.

--- src/roland/bin/roland-cli.js
+++ src/roland/bin/roland-cli.js
@@ -1,7 +1,14 @@
 #!/usr/bin/env node
 
 ;(function(){ // wrapper in case we're in module_context mode
+    var program, pack;
 
+    program = require('commander');
+    pack = require('../package.json');
+    program.version(pack.version);
     process.title = "roland"
 
+    //setup commands and options
+
+    program.parse(process.argv);
 })()
