f81f214012c457aa2e7bcdd000e7c14239937c7f
EisenbergEffect <rob@bluespire.com>
2013-06-20 16:50:40 -0400

    Fixing up line ending and allowing weyland bin.

---
+++ src/weyland/bin/cli.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+;(function(){ // wrapper in case we're in module_context mode
+    var program = require('commander'),
+        pack = require('../package.json'),
+        errorHandler = require('../lib/utils/errorHandler');
+
+    process.title = "weyland"
+    process.on("uncaughtException", errorHandler);
+
+    program.version(pack.version);
+
+    require('../lib/commands/build');
+
+    //display help if command is missing
+    if (process.argv.length === 2 || (process.argv.length > 2 && process.argv[2] === '--help')) {
+        process.argv[2] = '--help';
+    }
+
+    program.parse(process.argv);
+})()
