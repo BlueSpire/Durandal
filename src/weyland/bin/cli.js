ecfdeb4b9d7152a521c2957cea77038cd6ad0b0f
EisenbergEffect <rob@bluespire.com>
2013-06-20 20:30:22 -0400

    Some reorg of weyland.
    Apparently I wasn't thinking clearly when I set this up vefore.

--- src/weyland/bin/cli.js
+++ src/weyland/bin/cli.js
@@ -10,7 +10,7 @@
 
     program.version(pack.version);
 
-    require('../lib/commands/build');
+    require('../lib/commands/build/cli-adapter');
 
     //display help if command is missing
     if (process.argv.length === 2 || (process.argv.length > 2 && process.argv[2] === '--help')) {
---
+++ src/weyland/lib/commands/build/index.js
@@ -0,0 +1,3 @@
+exports.invoke = function(config){
+    console.log("Weyland 'build' doesn't do anything yet...");
+};
