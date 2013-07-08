7c4215812c46fc1d1c9e19fa4d97a191d8994909
EisenbergEffect <rob@bluespire.com>
2013-07-08 14:00:36 -0400

    Finished adding docs for the system module.

--- platforms/Mimosa/StarterKit/assets/javascripts/vendor/durandal/system.js
+++ platforms/Mimosa/StarterKit/assets/javascripts/vendor/durandal/system.js
@@ -3,6 +3,12 @@
  * Available via the MIT license.
  * see: http://durandaljs.com or https://github.com/BlueSpire/Durandal for details.
  */
+/**
+ * The system module encapsulates the most basic features used by other modules.
+ * @module system
+ * @requires require
+ * @requires jquery
+ */
 define(['require', 'jquery'], function(require, $) {
     var isDebugging = false,
         nativeKeys = Object.keys,
@@ -86,9 +92,27 @@ define(['require', 'jquery'], function(require, $) {
         throw new Error(error, id);
     };
 
+    /**
+     * @class SystemModule
+     * @static
+     */
     system = {
+        /**
+         * Durandal's version.
+         * @property {string} version
+         */
         version: "2.0.0-pre",
+        /**
+         * A noop function.
+         * @method noop
+         */
         noop: noop,
+        /**
+         * Gets the module id for the specified object.
+         * @method getModuleId
+         * @param {object} obj The object whose module id you wish to determine.
+         * @return {string} The module id.
+         */
         getModuleId: function(obj) {
             if (!obj) {
                 return null;
@@ -104,6 +128,12 @@ define(['require', 'jquery'], function(require, $) {
 
             return obj.__moduleId__;
         },
+        /**
+         * Sets the module id for the specified object.
+         * @method setModuleId
+         * @param {object} obj The object whose module id you wish to set.
+         * @param {string} id The id to set for the specified object.
+         */
         setModuleId: function(obj, id) {
             if (!obj) {
                 return;
@@ -120,6 +150,12 @@ define(['require', 'jquery'], function(require, $) {
 
             obj.__moduleId__ = id;
         },
+        /**
+         * Resolves the default object instance for a module. If the module is an object, the module is returned. If the module is a function, that function is called with `new` and it's result is returned.
+         * @method resolveObject
+         * @param {object} module The module to use to get/create the default object for.
+         * @return {object} The default object for the module.
+         */
         resolveObject: function(module) {
             if (system.isFunction(module)) {
                 return new module();
@@ -127,6 +163,12 @@ define(['require', 'jquery'], function(require, $) {
                 return module;
             }
         },
+        /**
+         * Gets/Sets whether or not Durandal is in debug mode.
+         * @method debug
+         * @param {boolean} [enable] Turns on/off debugging.
+         * @return {boolean} Whether or not Durandal is current debugging.
+         */
         debug: function(enable) {
             if (arguments.length == 1) {
                 isDebugging = enable;
@@ -139,26 +181,59 @@ define(['require', 'jquery'], function(require, $) {
                     this.log = noop;
                     this.error = noop;
                 }
-            } else {
-                return isDebugging;
             }
+
+            return isDebugging;
         },
+        /**
+         * Logs data to the console. Pass any number of parameters to be logged. Log output is not processed if the framework is not running in debug mode.
+         * @method log
+         * @param {object} info* The objects to log.
+         */
         log: noop,
+        /**
+         * Logs an error.
+         * @method error
+         * @param {string|Error} obj The error to report.
+         */
         error: noop,
+        /**
+         * Asserts a condition by throwing an error if the condition fails.
+         * @method assert
+         * @param {boolean} condition The condition to check.
+         * @param {string} message The message to report in the error if the condition check fails.
+         */
         assert: function (condition, message) {
             if (!condition) {
                 system.error(new Error(message || 'Assert:Failed'));
             }
         },
+        /**
+         * Creates a deferred object which can be used to create a promise. Optionally pass a function action to perform which will be passed an object used in resolving the promise.
+         * @method defer
+         * @param {function} [action] The action to defer. You will be passed the deferred object as a paramter.
+         * @return {Deferred} The deferred object.
+         */
         defer: function(action) {
             return $.Deferred(action);
         },
+        /**
+         * Creates a simple V4 UUID. This should not be used as a PK in your database. It can be used to generate internal, unique ids. For a more robust solution see [node-uuid](https://github.com/broofa/node-uuid).
+         * @method guid
+         * @return {string} The guid.
+         */
         guid: function() {
             return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
                 var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
                 return v.toString(16);
             });
         },
+        /**
+         * Uses require.js to obtain a module. This function returns a promise which resolves with the module instance. You can pass more than one module id to this function. If more than one is passed, then the promise will resolve with one callback parameter per module.
+         * @method acquire
+         * @param {string} moduleId* The id(s) of the modules to load.
+         * @return {Promise} A promise for the loaded module(s).
+         */
         acquire: function() {
             var modules = slice.call(arguments, 0);
             return this.defer(function(dfd) {
@@ -172,6 +247,12 @@ define(['require', 'jquery'], function(require, $) {
                 });
             }).promise();
         },
+        /**
+         * Extends the first object with the properties of the following objects.
+         * @method extend
+         * @param {object} obj The target object to extend.
+         * @param {object} extension* Uses to extend the target object.
+         */
         extend: function(obj) {
             var rest = slice.call(arguments, 1);
 
@@ -187,6 +268,12 @@ define(['require', 'jquery'], function(require, $) {
 
             return obj;
         },
+        /**
+         * Uses a setTimeout to wait the specified milliseconds.
+         * @method wait
+         * @param {number} milliseconds The number of milliseconds to wait.
+         * @return {Promise}
+         */
         wait: function(milliseconds) {
             return system.defer(function(dfd) {
                 setTimeout(dfd.resolve, milliseconds);
@@ -194,6 +281,12 @@ define(['require', 'jquery'], function(require, $) {
         }
     };
 
+    /**
+     * Gets all the owned keys of the specified object.
+     * @method keys
+     * @param {object} object The object whose owned keys should be returned.
+     * @return {string[]} The keys.
+     */
     system.keys = nativeKeys || function(obj) {
         if (obj !== Object(obj)) {
             throw new TypeError('Invalid object');
@@ -210,21 +303,87 @@ define(['require', 'jquery'], function(require, $) {
         return keys;
     };
 
+    /**
+     * Determines if the specified object is an html element.
+     * @method isElement
+     * @param {object} object The object to check.
+     * @return {boolean} True if matches the type, false otherwise.
+     */
     system.isElement = function(obj) {
         return !!(obj && obj.nodeType === 1);
     };
 
+    /**
+     * Determines if the specified object is an array.
+     * @method isArray
+     * @param {object} object The object to check.
+     * @return {boolean} True if matches the type, false otherwise.
+     */
     system.isArray = nativeIsArray || function(obj) {
         return toString.call(obj) == '[object Array]';
     };
 
+    /**
+     * Determines if the specified object is...an object. ie. Not an array, string, etc.
+     * @method isObject
+     * @param {object} object The object to check.
+     * @return {boolean} True if matches the type, false otherwise.
+     */
     system.isObject = function(obj) {
         return obj === Object(obj);
     };
 
+    /**
+     * Determines if the specified object is a boolean.
+     * @method isBoolean
+     * @param {object} object The object to check.
+     * @return {boolean} True if matches the type, false otherwise.
+     */
     system.isBoolean = function(obj) {
         return typeof(obj) === "boolean";
     };
+
+    /**
+     * Determines if the specified object is a function arguments object.
+     * @method isArguments
+     * @param {object} object The object to check.
+     * @return {boolean} True if matches the type, false otherwise.
+     */
+
+    /**
+     * Determines if the specified object is a function.
+     * @method isFunction
+     * @param {object} object The object to check.
+     * @return {boolean} True if matches the type, false otherwise.
+     */
+
+    /**
+     * Determines if the specified object is a string.
+     * @method isString
+     * @param {object} object The object to check.
+     * @return {boolean} True if matches the type, false otherwise.
+     */
+
+    /**
+     * Determines if the specified object is a number.
+     * @method isNumber
+     * @param {object} object The object to check.
+     * @return {boolean} True if matches the type, false otherwise.
+     */
+
+    /**
+     * Determines if the specified object is a date.
+     * @method isDate
+     * @param {object} object The object to check.
+     * @return {boolean} True if matches the type, false otherwise.
+     */
+
+    /**
+     * Determines if the specified object is a RegExp.
+     * @method isRegExp
+     * @param {object} object The object to check.
+     * @return {boolean} True if matches the type, false otherwise.
+     */
 
     //isArguments, isFunction, isString, isNumber, isDate, isRegExp.
     var isChecks = ['Arguments', 'Function', 'String', 'Number', 'Date', 'RegExp'];
