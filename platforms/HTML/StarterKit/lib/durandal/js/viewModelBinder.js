5f04f1fc953a5e4f7100c588c09580fe23446ee8
EisenbergEffect <rob@bluespire.com>
2013-07-09 01:47:02 -0400

    Core api now documented.

--- platforms/HTML/StarterKit/lib/durandal/js/viewModelBinder.js
+++ platforms/HTML/StarterKit/lib/durandal/js/viewModelBinder.js
@@ -3,6 +3,12 @@
  * Available via the MIT license.
  * see: http://durandaljs.com or https://github.com/BlueSpire/Durandal for details.
  */
+/**
+ * The viewLocator module collaborates with the viewEngine module to provide views (literally dom sub-trees) to other parts of the framework as needed. The primary consumer of the viewLocator is the composition module.
+ * @module viewModelBinder
+ * @requires system
+ * @requires knockout
+ */
 define(['durandal/system', 'knockout'], function (system, ko) {
     var viewModelBinder,
         insufficientInfoMessage = 'Insufficient Information to Bind',
@@ -82,13 +88,49 @@ define(['durandal/system', 'knockout'], function (system, ko) {
         }
     }
 
+    /**
+     * @class ViewModelBinderModule
+     * @static
+     */
     return viewModelBinder = {
+        /**
+         * Called before every binding operation. Does nothing by default.
+         * @method beforeBind
+         * @param {object} data The data that is about to be bound.
+         * @param {DOMElement} view The view that is about to be bound.
+         * @param {object} instruction The object that carries the binding instructions.
+         */
         beforeBind: system.noop,
+        /**
+         * Called after every binding operation. Does nothing by default.
+         * @method afterBind
+         * @param {object} data The data that has just been bound.
+         * @param {DOMElement} view The view that has just been bound.
+         * @param {object} instruction The object that carries the binding instructions.
+         */
         afterBind: system.noop,
+        /**
+         * Indicates whether or not the binding system should throw errors or not.
+         * @property {boolean} throwOnErrors
+         * @default false The binding system will not throw errors by default. Instead it will log them.
+         */
         throwOnErrors: false,
+        /**
+         * Gets the binding instruction that was associated with a view when it was bound.
+         * @method getBindingInstruction
+         * @param {DOMElement} view The view that was previously bound.
+         * @return {object} The object that carries the binding instructions.
+         */
         getBindingInstruction:function(view){
             return ko.utils.domData.get(view, bindingInstructionKey);
         },
+        /**
+         * Binds the view, preserving the existing binding context. Optionally, a new context can be created, parented to the previous context.
+         * @method bindContext
+         * @param {KnockoutBindingContext} bindingContext The current binding context.
+         * @param {DOMElement} view The view to bind.
+         * @param {object} [obj] The data to bind to, causing the creation of a child binding context if present.
+         */
         bindContext: function(bindingContext, view, obj) {
             if (obj) {
                 bindingContext = bindingContext.createChildContext(obj);
@@ -96,6 +138,12 @@ define(['durandal/system', 'knockout'], function (system, ko) {
 
             return doBind(obj, view, bindingContext, obj || bindingContext.$data);
         },
+        /**
+         * Binds the view, preserving the existing binding context. Optionally, a new context can be created, parented to the previous context.
+         * @method bind
+         * @param {object} obj The data to bind to.
+         * @param {DOMElement} view The view to bind.
+         */
         bind: function(obj, view) {
             return doBind(obj, view, obj, obj);
         }
