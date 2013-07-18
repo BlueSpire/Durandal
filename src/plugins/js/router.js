9dabd55e0d0a22296a5ccddb654c953ce1efc373
EisenbergEffect <rob@bluespire.com>
2013-07-18 14:57:06 -0400

    Making the router.reset remove all event handlers.

--- src/plugins/js/router.js
+++ src/plugins/js/router.js
@@ -689,6 +689,7 @@ define(['durandal/system', 'durandal/app', 'durandal/activator', 'durandal/event
             currentInstruction = currentActivation = undefined;
             router.handlers = [];
             router.routes = [];
+            router.off();
             delete router.options;
         };
 
