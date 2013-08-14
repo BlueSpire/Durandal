9eec266c4203e5adda1948697d7cad61342aee2e
EisenbergEffect <rob@bluespire.com>
2013-08-14 12:00:11 -0400

    Added weyland-config where appropriate.

---
+++ platforms/HTML/StarterKit/weyland-config.js
@@ -0,0 +1,39 @@
+exports.config = function(weyland) {
+    weyland.build('main')
+        .task.jshint({
+            include:'app/**/*.js'
+        })
+        .task.uglifyjs({
+            include:['app/**/*.js', 'lib/durandal/**/*.js']
+        })
+        .task.rjs({
+            include:['app/**/*.{js,html}', 'lib/durandal/**/*.js'],
+            loaderPluginExtensionMaps:{
+                '.html':'text'
+            },
+            rjs:{
+                name:'../lib/require/almond-custom', //to deploy with require.js, use the build's name here instead
+                insertRequire:['main'], //not needed for require
+                baseUrl : 'app',
+                mainConfigFile:'app/main.js', //not needed for require
+                wrap:true, //not needed for require
+                paths : {
+                    'text': '../lib/require/text',
+                    'durandal':'../lib/durandal/js',
+                    'plugins' : '../lib/durandal/js/plugins',
+                    'transitions' : '../lib/durandal/js/transitions',
+                    'knockout': '../lib/knockout/knockout-2.3.0',
+                    'bootstrap': '../lib/bootstrap/js/bootstrap',
+                    'jquery': '../lib/jquery/jquery-1.9.1'
+                },
+                inlineText: true,
+                optimize : 'none',
+                pragmas: {
+                    build: true
+                },
+                stubModules : ['text'],
+                keepBuildDir: true,
+                out:'app/main-built.js'
+            }
+        });
+}
