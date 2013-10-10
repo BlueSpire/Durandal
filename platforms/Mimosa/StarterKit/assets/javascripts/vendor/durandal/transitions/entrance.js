07eb5ec16d966890fa7b280177a97870765275c7
EisenbergEffect <rob@bluespire.com>
2013-10-09 22:49:43 -0400

    Updated build and changes doc.

--- platforms/Mimosa/StarterKit/assets/javascripts/vendor/durandal/transitions/entrance.js
+++ platforms/Mimosa/StarterKit/assets/javascripts/vendor/durandal/transitions/entrance.js
@@ -60,14 +60,18 @@ define(['durandal/system', 'durandal/composition', 'jquery'], function(system, c
                     var $child = $(context.child);
 
                     $child.css(startValues);
-                    $child.animate(endValues, duration, 'swing', function () {
-                        $child.css(clearValues);
-                        endTransition();
+                    $child.animate(endValues, {
+                        duration: duration,
+                        easing: 'swing',
+                        always: function () {
+                            $child.css(clearValues);
+                            endTransition();
+                        }
                     });
                 }
 
                 if (context.activeView) {
-                    $(context.activeView).fadeOut(fadeOutDuration, startTransition);
+                    $(context.activeView).fadeOut({ duration: fadeOutDuration, always: startTransition });
                 } else {
                     startTransition();
                 }
