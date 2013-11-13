2c91d67fd3c0de97c3bb9ce15e8ce5c40b334b59
EisenbergEffect <rob@bluespire.com>
2013-11-13 10:54:27 -0500

    Fix Issue #372: Better system.guid implementation

--- src/durandal/js/system.js
+++ src/durandal/js/system.js
@@ -218,9 +218,11 @@ define(['require', 'jquery'], function(require, $) {
          * @return {string} The guid.
          */
         guid: function() {
-            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-                var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-                return v.toString(16);
+            var d = new Date().getTime();
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+                var r = (d + Math.random() * 16) % 16 | 0;
+                d = Math.floor(d/16);
+                return (c == 'x' ? r : (r & 0x7 | 0x8)).toString(16);
             });
         },
         /**
