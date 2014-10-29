12bd78651ae8ffa5386c16ec4949a3d6cd2a17dc
Nic Mendoza <nic.mendoza@gmail.com>
2014-10-29 13:59:01 -0400

    Fix issue #556: issue with objects in activeInstruction params

--- src/plugins/js/router.js
+++ src/plugins/js/router.js
@@ -597,7 +597,7 @@ define(['durandal/system', 'durandal/app', 'durandal/activator', 'durandal/event
 
             if(router.relativeToParentRouter){
                 var instruction = this.parent.activeInstruction();
-				coreFragment = queryIndex == -1 ? instruction.params.join('/') : instruction.params.slice(0, -1).join('/');
+				coreFragment = (queryIndex == -1 ? instruction.params : instruction.params.slice(0, -1)).filter(function(param){return typeof param === 'string'}).join('/');
 
                 if(coreFragment && coreFragment.charAt(0) == '/'){
                     coreFragment = coreFragment.substr(1);
