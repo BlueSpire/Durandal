9fef47e339725bf0691ee8012cd87353d647ff60
mryellow <mr-yellow@mr-yellow.com>
2015-02-26 13:21:33 +1100

    fixes #622

--- src/starterkit/app/viewmodels/welcome.js
+++ src/starterkit/app/viewmodels/welcome.js
@@ -1,4 +1,4 @@
-﻿define(function() {
+﻿define(['durandal/app'], function (app) {
     var ctor = function () {
         this.displayName = 'Welcome to the Durandal Starter Kit!';
         this.description = 'Durandal is a cross-device, cross-platform client framework written in JavaScript and designed to make Single Page Applications (SPAs) easy to create and maintain.';
